7417293623f8026961629457f907f3c3d3d2f172
JuanAira <juanaira_01@hotmail.com>
2022-02-02 15:23:09 -0300

    Add component auth

--- section-18/src/components/auth/LoginScreen.js
+++ section-18/src/components/auth/LoginScreen.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { startLoginEmailPassword, startGoogleLogin } from '../../actions/auth';
 
 export const LoginScreen = () => {
+    const dispatch = useDispatch();
+    const { loading } = useSelector(state => state.ui);
+
+    const [formValues, handleInputChange] = useForm({
+        email: 'juanga@gmail.com',
+        password: '123456'
+    });
+
+    const { email, password } = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(startLoginEmailPassword(email, password));
+    }
+
+    const handleGoogleLogin = () => {
+        dispatch(startGoogleLogin());
+    }
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
 
-            <form>
+            <form onSubmit={handleLogin}>
 
-                <input 
+                <input
                     type="text"
                     placeholder="Email"
                     name="email"
@@ -16,7 +38,7 @@ export const LoginScreen = () => {
                     autoComplete="off"
                 />
 
-                <input 
+                <input
                     type="password"
                     placeholder="Password"
                     name="password"
@@ -27,16 +49,19 @@ export const LoginScreen = () => {
                 <button
                     type="submit"
                     className="btn btn-primary btn-block"
+                    disabled={loading}
                 >
                     Login
                 </button>
 
-                
+
                 <div className="auth__social-networks">
                     <p>Login with social networks</p>
 
-                    <div 
+                    <div
                         className="google-btn"
+                        onClick={handleGoogleLogin}
+                        disabled={loading}
                     >
                         <div className="google-icon-wrapper">
                             <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
@@ -47,11 +72,11 @@ export const LoginScreen = () => {
                     </div>
                 </div>
 
-                <Link 
+                <Link
                     to="/auth/register"
                     className="link"
                 >
-                    Create new account    
+                    Create new account
                 </Link>
 
             </form>
